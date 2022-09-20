14bb6bd16ed7e02c1b137d75ab2508d922ec1409
valentsk <skredovavalenti@mail.ru>
2022-09-20 17:51:15 +0300

    Resolve task 15

--- Homework002/Ex015weekDay/Program.cs
+++ Homework002/Ex015weekDay/Program.cs
@@ -4,3 +4,35 @@
 // 7 -> да
 // 1 -> нет
 
+Console.Clear();
+
+int day = -1;
+while (true)
+{
+    try
+    {
+        Console.Write("Введите день недели: ");
+        day = int.Parse(Console.ReadLine()!);
+        if (day > 0 && day < 8)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Некорректный ввод, введите день недели от 1 до 7");
+        }
+    }
+    catch
+    {
+        Console.WriteLine("Некорректный ввод (символы, строки), введите день недели от 1 до 7");
+    }
+}
+
+if (day > 5)
+{
+    Console.WriteLine("Это выходной день");
+}
+else
+{
+    Console.WriteLine("Это не выходной день");
+}
