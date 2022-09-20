2a18451fe6ffa3398bd83f569780971506a2f65e
valentsk <skredovavalenti@mail.ru>
2022-09-20 17:05:51 +0300

    Resolve task 10

--- Homework002/Ex010secondDigit/Program.cs
+++ Homework002/Ex010secondDigit/Program.cs
@@ -2,3 +2,30 @@
 // 456 -> 5
 // 782 -> 8
 // 918 -> 1
+
+Console.Clear();
+
+int number = -1;
+while (true)
+{
+    try
+    {
+        Console.Write("Введите трехзначное число: ");
+        number = Math.Abs(int.Parse(Console.ReadLine()!));
+        if (number > 99 && number < 1000)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Некорректный ввод, введите трехзначное число");
+        }
+    }
+    catch
+    {
+        Console.WriteLine("Некорректный ввод (символы, строки), введите трехзначное число");
+    }
+}
+
+int secondDigit = number % 100 / 10;
+Console.WriteLine($"Вторая цифра: {secondDigit}");
