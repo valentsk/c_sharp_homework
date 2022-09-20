b5cee0f6c219d2b8f2068f0cdf97e84e1eaf858c
valentsk <skredovavalenti@mail.ru>
2022-09-20 17:44:36 +0300

    Resolve task 13 with string data

--- Homework002/Ex013thirdDigit/Program.cs
+++ Homework002/Ex013thirdDigit/Program.cs
@@ -2,3 +2,30 @@
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
+
+Console.Clear();
+
+int number = -1;
+while (true)
+{
+    try
+    {
+        Console.Write("Введите число: ");
+        number = Math.Abs(int.Parse(Console.ReadLine()!));
+        break;
+    }
+    catch
+    {
+        Console.WriteLine("Некорректный ввод (символы, строки), введите число");
+    }
+}
+
+if (number < 100)
+{
+    Console.WriteLine($"Третьей цифры нет");
+}
+else
+{
+    string numberA = Convert.ToString(number);
+    Console.WriteLine($"Третья цифра: {numberA[2]}");
+}
