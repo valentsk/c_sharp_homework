fc79c6dc44a1e4eb311d118f2621e2d9349082f2
valentsk <skredovavalenti@mail.ru>
2022-09-24 23:40:51 +0300

    Resolve ex 019 palindrom

--- Homework003/Ex019palindrom/Program.cs
+++ Homework003/Ex019palindrom/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Clear();
+
+int number = -1;
+while (true)
+{
+    try
+    {
+        Console.Write("Введите пятизначное число: ");
+        number = Math.Abs(int.Parse(Console.ReadLine()!));
+        if (number > 9999 && number < 100000)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Некорректный ввод, введите пятизначное число");
+        }
+    }
+    catch
+    {
+        Console.WriteLine("Некорректный ввод (символы, строки), введите пятизначное число");
+    }
+}
+int firstDigit = number / 10000;
+int fifthDigit = number % 10;
+int secondDigit = number / 1000 % 10;
+int fourthDigit = number / 10 % 10;
+
+if ((firstDigit == fifthDigit) && (secondDigit == fourthDigit))
+{
+    Console.Write("Число является палиндромом");
+}
+else
+{
+    Console.Write("Число не является палиндромом");
+}
