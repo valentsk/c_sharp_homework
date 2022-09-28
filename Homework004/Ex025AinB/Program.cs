81ac917b77c0056a4d50ffc900e441809d445ec1
valentsk <skredovavalenti@mail.ru>
2022-09-28 23:33:58 +0300

    Resolve task 025

--- Homework004/Ex025AinB/Program.cs
+++ Homework004/Ex025AinB/Program.cs
@@ -5,3 +5,41 @@
 // - Нельзя использовать класс Math
 // - Должна быть отдельная функция Power, которая возвразает результат
 // - Внутри функций НЕЛЬЗЯ использовать класс Console. Только в основном блоке кода
+
+int Power(int number1, int number2)
+{
+    int result = number1;
+    for (int i = 1; i < number2; i++)
+    {
+        result *= number1;
+    }
+    return result;
+}
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int numberA = int.Parse(Console.ReadLine()!);
+int numberB = -1;
+while (true)
+{
+    try
+    {
+        Console.Write("Введите натуральную степень числа: ");
+        numberB = int.Parse(Console.ReadLine()!);
+        if (numberB > 0)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Некорректный ввод, введите натуральную степень числа (1, 2, 3, ...): ");
+        }
+    }
+    catch
+    {
+        Console.WriteLine("Некорректный ввод (символы, строки), введите натуральную степень числа (1, 2, 3, ...): ");
+    }
+}
+
+Console.WriteLine($"Результат возведения числа {numberA} в степень {numberB} равен {Power(numberA, numberB)}");
