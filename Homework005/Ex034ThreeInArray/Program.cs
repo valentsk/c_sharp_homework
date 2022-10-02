2213f8c02ef0d59e60e1c66cbd83a86ef85c66ee
valentsk <skredovavalenti@mail.ru>
2022-10-02 20:27:25 +0300

    Resolve task 034

--- Homework005/Ex034ThreeInArray/Program.cs
+++ Homework005/Ex034ThreeInArray/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] CreateArray()
+{
+    Console.Write("Введи размерность массива: ");
+    int size = int.Parse(Console.ReadLine()!);
+    int[] array = new int[size];
+    return array;
+}
+
+void FillArray(int[] array)
+{
+    Random randGenerator = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = randGenerator.Next(100, 1000);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("Массив: ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+int CountEvenNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.Clear();
+int[] array = CreateArray();
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+int countEven = CountEvenNumbers(array);
+Console.WriteLine($"Количество четных чисел в массиве: {countEven}");
