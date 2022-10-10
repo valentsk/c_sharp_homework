1c9c7e215b923c0d8f9aa9453b148fcffa54db74
valentsk <skredovavalenti@mail.ru>
2022-10-10 18:20:16 +0300

    Add empty folders with tasks

---
+++ Homework007/Ex047/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+double[,] array = new double[3, 4];
+FillArray(array);
+PrintArray(array);
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void FillArray(double[,] array)
+{
+    Random generator = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Math.Round(generator.NextDouble()*generator.Next(-9,10),1);
+        }
+    }
+}
