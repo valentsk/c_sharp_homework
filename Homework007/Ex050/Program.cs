ae402cc2887e5cb8f9688565f5457263395ff49d
valentsk <skredovavalenti@mail.ru>
2022-10-10 18:31:27 +0300

    Resolve ex 050

--- Homework007/Ex050/Program.cs
+++ Homework007/Ex050/Program.cs
@@ -5,3 +5,49 @@
 // 8 4 2 4
 // 1
 // 7 -> такого числа в массиве нет
+
+Console.Clear();
+int[,] array = new int[5, 5];
+FillArray(array);
+PrintArray(array);
+
+Console.Write("Введите индекс по строкам: ");
+int indexI = int.Parse(Console.ReadLine()!);
+Console.Write("Введите индекс по столбцам: ");
+int indexJ = int.Parse(Console.ReadLine()!);
+FindNumber(indexI, indexJ, array);
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void FillArray(int[,] array)
+{
+    Random generator = new Random();
+   for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = generator.Next(-9,10);
+        }
+    }
+}
+
+void FindNumber(int indexI, int indexJ, int[,] array)
+{
+   if ((indexI <= array.GetLength(0)) && (indexJ <= array.GetLength(1)))
+   {
+        Console.Write(array[indexI, indexJ]);
+   }
+   else
+   {
+        Console.Write("Такого элемента нет");
+   }
+}
--- Homework007/Ex052/Program.cs
+++ Homework007/Ex052/Program.cs
@@ -4,3 +4,50 @@
 // 5 9 2 3
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.Write("Введите m: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите n: ");
+int n = int.Parse(Console.ReadLine()!);
+
+
+int[,] array = new int[m, n];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+AvarageArray(array);
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] array)
+{
+     Random generator = new Random();
+   for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = generator.Next(-9,10);
+        }
+    }
+}
+
+void AvarageArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i += 2)
+    {
+        for (int j = 0; j < array.GetLength(1); j += 2)
+        {
+            array[i,j] = array[i,j] * array[i,j];
+        }
+    }
+}
