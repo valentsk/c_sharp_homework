0b684c43559768b5fcaff3848c1acc5e5cdd6901
valentsk <skredovavalenti@mail.ru>
2022-10-11 16:10:26 +0300

    Resolve task 054

--- Homework008/Ex054/Program.cs
+++ Homework008/Ex054/Program.cs
@@ -8,3 +8,65 @@
 // 7 4 2 1
 // 9 5 3 2
 // 8 4 4 2
+
+
+Console.Write("Введите m: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите n: ");
+int n = int.Parse(Console.ReadLine()!);
+
+
+int[,] array = new int[m, n];
+
+Console.WriteLine("Исходный массив: ");
+FillArray(array);
+PrintArray(array);
+Console.WriteLine("Сортированный массив: ");
+SortArray(array);
+PrintArray(array);
+
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] array)
+{
+    Random generator = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = generator.Next(0, 10);
+        }
+    }
+}
+
+void SortArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int indexMin = 0; indexMin < array.GetLength(1) - 1 - j; indexMin++)
+            {
+                if (array[i, indexMin] < array[i, indexMin + 1])
+                {
+                    int temp = array[i, indexMin];
+                    array[i, indexMin] = array[i, indexMin + 1];
+                    array[i, indexMin + 1] = temp;
+                }
+            }
+        }
+    }
+}
+
+
