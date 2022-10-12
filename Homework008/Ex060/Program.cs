29f022057b5443bf96ab5fd77b82572f2d4bc8d3
valentsk <skredovavalenti@mail.ru>
2022-10-12 16:49:09 +0300

    Resolve ex 060 with Random

--- Homework008/Ex060/Program.cs
+++ Homework008/Ex060/Program.cs
@@ -8,9 +8,21 @@
 
 Console.Clear();
 
-int[,,] array = new int[2, 2, 2];
-FillArray(array);
-PrintArray(array);
+int[,,] array = new int[2, 2, 2]; // создаем массив
+FillArray(array); // заполняем
+bool resultBool = ThirdMatrixInOneArrayDontRepeat(array); // трехмерный массив в одномерный c сортировкой и проверкой повторных чисел
+PrintRandom(resultBool); // печатает массив, если не повторяются значение
+
+bool ThirdMatrixInOneArrayDontRepeat(int[,,] array)
+{
+    int[] rowArray = MatrixToRow(array); // трехмерный массив в одномерный
+    SortArray(rowArray); // сортируем
+    // PrintOneArray(rowArray); // смотрим сортированный
+    Console.WriteLine();
+    bool resBool = GetFrequense(rowArray);
+    return resBool; // если повторяются символы, то должен Фолс
+}
+
 
 void PrintArray(int[,,] array)
 {
@@ -26,6 +38,7 @@ void PrintArray(int[,,] array)
         }
     }
 }
+
 void FillArray(int[,,] array)
 {
     Random generator = new Random();
@@ -39,5 +52,83 @@ void FillArray(int[,,] array)
                 array[i, j, k] = generator.Next(0, 100);
             }
         }
+    }
+}
+
+int[] MatrixToRow(int[,,] matrix)
+{
+    int[] result = new int[matrix.GetLength(0) * matrix.GetLength(1) * matrix.GetLength(2)];
+    int index = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                result[index] = matrix[i, j, k];
+                index++;
+            }
+        }
+    }
+    return result;
+}
+
+void SortArray(int[] inArray)
+{
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        for (int j = i + 1; j < inArray.Length; j++)
+        {
+            if (inArray[i] > inArray[j])
+            {
+                int k = inArray[i];
+                inArray[i] = inArray[j];
+                inArray[j] = k;
+            }
+        }
+    }
+}
+
+void PrintOneArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+
+    }
+}
+
+bool GetFrequense(int[] sortedArray)
+{
+    bool result = true;
+    int currentElement = sortedArray[0];
+    int count = 1;
+    for (int i = 1; i < sortedArray.Length; i++)
+    {
+        if (sortedArray[i] == currentElement)
+        {
+            currentElement = sortedArray[i];
+            return false;
+        }
+        else
+        {
+            currentElement = sortedArray[i];
+        }
+    }
+    return true;
+}
+
+void PrintRandom(bool resultBoolean)
+{
+    if (resultBoolean == true)
+    {
+        PrintArray(array);
+    }
+    else
+    {
+        // Console.WriteLine("Еще раз"); смотрим запустились ли по новой методы 
+        FillArray(array);
+        bool resBool = ThirdMatrixInOneArrayDontRepeat(array); 
+        PrintRandom(resBool);
     }
 }
--- Homework008/Ex062/Program.cs
+++ Homework008/Ex062/Program.cs
@@ -4,3 +4,51 @@
 // 12 13 14 05
 // 11 16 15 06
 // 10 09 08 07
+
+Console.Clear();
+
+int[,] arrayOne = new int[4, 4];
+FillArray(arrayOne);
+PrintArray(arrayOne);
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] array)
+{
+    Random generator = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = generator.Next(1, 10);
+        }
+    }
+}
+
+
+int[,] ProductMatrix(int[,] arrayOne, int[,] arrayTwo)
+{
+    int[,] arrayProduct = new int[arrayOne.GetLength(0), arrayTwo.GetLength(1)];
+
+    for (int i = 0; i < arrayProduct.GetLength(0); i++)
+    {
+        for (int j = 0; j < arrayProduct.GetLength(1); j++)
+        {
+            for (int k = 0; k < arrayOne.GetLength(0); k++)
+            {
+                arrayProduct[i, j] += arrayOne[i, k] * arrayTwo[k, j];
+            }
+        }
+    }
+    return arrayProduct;
+}
