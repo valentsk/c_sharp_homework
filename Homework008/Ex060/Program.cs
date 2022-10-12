36eda6db15dc0a9a6b39cb35c38970f748ec3918
valentsk <skredovavalenti@mail.ru>
2022-10-12 15:43:47 +0300

    Resolve ex 060

--- Homework008/Ex060/Program.cs
+++ Homework008/Ex060/Program.cs
@@ -5,3 +5,39 @@
 // 34(1,0,0) 41(1,1,0)
 // 27(0,0,1) 90(0,1,1)
 // 26(1,0,1) 55(1,1,1)
+
+Console.Clear();
+
+int[,,] array = new int[2, 2, 2];
+FillArray(array);
+PrintArray(array);
+
+void PrintArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($"{array[i, j, k]}({i},{j},{k}) ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+void FillArray(int[,,] array)
+{
+    Random generator = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] = generator.Next(0, 100);
+            }
+        }
+    }
+}
