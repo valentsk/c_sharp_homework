7a0b1754080b45eb951b572f3c75071662c34926
valentsk <skredovavalenti@mail.ru>
2022-10-12 15:35:48 +0300

    Resolve ex 058

--- Homework008/Ex058/Program.cs
+++ Homework008/Ex058/Program.cs
@@ -5,3 +5,72 @@
 // Результирующая матрица будет:
 // 18 20
 // 15 18
+
+Console.Clear();
+
+Console.WriteLine("Первый массив:");
+Console.Write("Введите число строк: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число столбцов: ");
+int n = int.Parse(Console.ReadLine()!);
+int[,] arrayOne = new int[m, n];
+FillArray(arrayOne);
+PrintArray(arrayOne);
+
+Console.WriteLine();
+
+Console.WriteLine("Второй массив:");
+Console.WriteLine("Для умножения матриц число столбцов первой матрицы совпадает с числом строк второй матрицы");
+Console.Write("Введите число столбцов: ");
+int n2 = int.Parse(Console.ReadLine()!);
+int[,] arrayTwo = new int[n, n2];
+Console.WriteLine("Второй массив: ");
+FillArray(arrayTwo);
+PrintArray(arrayTwo);
+
+Console.WriteLine();
+Console.WriteLine("Произведение массивов:");
+PrintArray(ProductMatrix(arrayOne, arrayTwo));
+
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] array)
+{
+    Random generator = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = generator.Next(1, 10);
+        }
+    }
+}
+
+
+int[,] ProductMatrix(int[,] arrayOne, int[,] arrayTwo)
+{
+    int[,] arrayProduct = new int[arrayOne.GetLength(0), arrayTwo.GetLength(1)];
+
+    for (int i = 0; i < arrayProduct.GetLength(0); i++)
+    {
+        for (int j = 0; j < arrayProduct.GetLength(1); j++)
+        {
+            for (int k = 0; k < arrayOne.GetLength(0); k++)
+            {
+                arrayProduct[i, j] += arrayOne[i, k] * arrayTwo[k, j];
+            }
+        }
+    }
+    return arrayProduct;
+}
