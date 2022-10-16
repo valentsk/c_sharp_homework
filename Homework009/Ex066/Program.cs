a883d46f6ea3ec6fccfe8b62b1980771c9d47457
valentsk <skredovavalenti@mail.ru>
2022-10-16 14:27:29 +0300

    Resolve ex66

--- Homework009/Ex066/Program.cs
+++ Homework009/Ex066/Program.cs
@@ -3,3 +3,25 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
+Console.Clear();
+
+Console.Write("Введите M: ");
+int numberM = int.Parse(Console.ReadLine()!);
+Console.Write("Введите N: ");
+int numberN = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(RecSum(numberM, numberN));
+
+int RecSum(int numberM, int numberN, int sum = 0)
+{
+    if (numberM <= numberN)
+    {
+        sum += numberM;
+        numberM += 1;
+        return RecSum(numberM, numberN, sum);
+    }
+    else
+    {
+        return  sum;
+    }
+}
